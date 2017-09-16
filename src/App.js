3ad0248d4f3160318fd76009225c50aec2f3cb58
Mantas Kuncevičius <m.kuncevicius@gmail.com>
2017-09-16 14:13:01 +0300

    feat: getting books data from the API. Data is filtered and maped to show it in the specific bookshelf.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import SearchBook from './SearchBook'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-
 class BooksApp extends React.Component {
+
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+
+
 
   render() {
     return (
@@ -20,9 +31,21 @@ class BooksApp extends React.Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-              <BookShelf/>
-              <BookShelf/>
-              <BookShelf/>
+              <BookShelf
+                bookshelfTitle={"Currently Reading"}
+                selectedBookshelf={"currentlyReading"}
+                books={this.state.books}
+              />
+              <BookShelf 
+                bookshelfTitle={"Want to Read"}
+                selectedBookshelf={"wantToRead"}
+                books={this.state.books}
+              />
+              <BookShelf
+                bookshelfTitle={"Read"}
+                selectedBookshelf={"read"}
+                books={this.state.books}
+              />
             </div>
             <div className="open-search">
               <Link to="/search">Add a book</Link>
